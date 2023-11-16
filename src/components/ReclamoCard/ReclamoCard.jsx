1f5be1ed2e924fe6c90f048129e84471d386c16e
unknown <castrolucia2108@gmail.com>
2023-11-15 21:39:08 -0300

    filtros de reclamos

--- src/components/ReclamoCard/ReclamoCard.jsx
+++ src/components/ReclamoCard/ReclamoCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './ReclamoCard.scss'
 
-const ReclamoCard = ({admin, numeroEdificio, piso, numeroUnidad, descrip, documento}) => {
+const ReclamoCard = ({admin, numero, numeroEdificio, piso, numeroUnidad, descrip, documento}) => {
     return (
         <>
             <div className="card">
@@ -9,20 +9,13 @@ const ReclamoCard = ({admin, numeroEdificio, piso, numeroUnidad, descrip, docume
                     <div className="card__container_bg desestimado"></div>
 
                     <div className="card__container_title">
-                        N°: 23 - Nombre de reclamo
+                        N°: {numero} - Nombre de reclamo
                     </div>
 
                     <p className='card__container_desc'>Edificio: {numeroEdificio}</p>
                     <p className='card__container_desc'>Unidad: piso {piso} - dpto {numeroUnidad}</p>
                     <p className='card__container_desc'>Descripción: {descrip}</p>
                     {admin && <p className='card__container_desc'>Persona: {documento}</p>}
-
-                    <div className="card__container_date-box">
-                        Fecha de reclamo:
-                        <span className="card__container_date">
-                            04.11.2022
-                        </span>
-                    </div>
                 </div>
             </div>
         </>
--- src/pages/AdminReclamo.jsx
+++ src/pages/AdminReclamo.jsx
@@ -2,8 +2,12 @@ import React, { useState } from 'react'
 import './AdminReclamo.scss'
 import Navbar from '../components/Navbar/Navbar'
 import ReclamoCard from '../components/ReclamoCard/ReclamoCard'
+import Boton from '../components/Boton/Boton'
+import Loader from '../components/Loader/Loader'
 
 const AdminReclamo = () => {
+    const [reclamos, setReclamos] = useState([])
+    const [loading, setLoading] = useState(false)
 
     const [form, setForm] = useState({
         tipo: "",
@@ -15,32 +19,93 @@ const AdminReclamo = () => {
         setForm({ ...form, [name]: value })
     }
 
-  return (
-    <>
-        <Navbar options={['home', 'reclamos', 'unidades']} admin={true}/>
-        <main className='reclamos'>
-            <form action="">
-                <h2>Filtrar por:</h2>
-                <select name="tipo" id="tipo" onChange={handleInput} value={form.tipo}>
-                    <option value="">Tipo de filtro</option>
-                    <option value="edificio">Edificio</option>
-                    <option value="numero">Número</option>
-                    <option value="persona">Persona</option>
-                </select>
-                <input name='dato' type="text" value={form.dato} onChange={handleInput}/>
-            </form>
-            <div className="reclamos__table">
-                <ReclamoCard admin={true} />
-                <ReclamoCard admin={true} />
-                <ReclamoCard admin={true} />
-                <ReclamoCard admin={true} />
-                <ReclamoCard admin={true} />
-                <ReclamoCard admin={true} />
-                <ReclamoCard admin={true} />
-            </div>
-        </main>
-    </>
-  )
+    async function fetchReclamos(url) {
+        try {
+            setLoading(true)
+            const response = await fetch(url)
+            if (!response.ok) {
+                throw new Error('error')
+            }
+            const data = await response.json()
+            setReclamos(data)
+            setLoading(false)
+        } catch (error) {
+            console.error("Error:", error)
+            setLoading(false)
+        }
+    }
+
+    const handleSumbit = (e) => {
+        e.preventDefault()
+        let numero
+
+        switch (form.tipo) {
+            case "edificio":
+                numero = parseInt(form.dato)
+                fetchReclamos(`https://localhost:8080/reclamos/edificio/${form.dato}`)
+                break;
+
+            case "numero":
+                numero = parseInt(form.dato)
+                fetchReclamos(`https://localhost:8080/reclamos/${form.dato}`)
+                break;
+
+            case "persona":
+                fetchReclamos(`https://localhost:8080/reclamos/persona:${form.dato}`)
+                break;
+
+            default:
+                break;
+        }
+    }
+
+
+    return (
+        <>
+            <Navbar options={['home', 'reclamos', 'unidades']} admin={true} />
+            <main className='reclamos'>
+                <form className='reclamos__table'>
+                    <h2 className='reclamos__table__title'>Filtrar por:</h2>
+                    <select className='reclamos__table__filter' name="tipo" id="tipo" onChange={handleInput} value={form.tipo}>
+                        <option value="">Tipo de filtro</option>
+                        <option value="edificio">Edificio</option>
+                        <option value="numero">Número</option>
+                        <option value="persona">Persona</option>
+                    </select>
+                    <input className='reclamos__table__filter' name='dato' type="text" value={form.dato} onChange={handleInput} />
+                    <Boton action={e => handleSumbit(e)} msg={"Filtrar"} />
+                </form>
+                <div className="reclamos__table">
+                    {loading ? <Loader /> :
+                        (
+                            Array.isArray(reclamos) ?
+                                reclamos.map((reclamo) => (
+                                    <ReclamoCard
+                                        admin={true}
+                                        numero={reclamo.numero}
+                                        numeroEdificio={reclamo.edificio.codigo}
+                                        piso={reclamo.unidad.piso}
+                                        numeroUnidad={reclamo.unidad.numero}
+                                        descrip={reclamo.descripcion}
+                                        documento={reclamo.usuario.documento}
+                                        key={reclamo.numero}
+                                    />
+                                )) :
+                                <ReclamoCard
+                                    admin={true}
+                                    numero={reclamos.numero}
+                                    numeroEdificio={reclamos.edificio.codigo}
+                                    piso={reclamos.unidad.piso}
+                                    numeroUnidad={reclamos.unidad.numero}
+                                    descrip={reclamos.descripcion}
+                                    documento={reclamos.usuario.documento}
+                                    key={reclamos.numero}
+                                />
+                        )}
+                </div>
+            </main>
+        </>
+    )
 }
 
 export default AdminReclamo
