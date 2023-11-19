9d78237f0497579bf21331ec21bd34ebabf74836
Gianfranco Impedovo <impefranco98@gmail.com>
2023-11-19 18:52:33 -0300

    agrego la carga de localstorage en la app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useEffect } from 'react';
 import './App.scss';
 import Routes from './router/Routes';
+import { useClient } from './context/UseContext';
 
 function App() {
+
+  const { setClient } = useClient();
+
+  useEffect(() => {
+    async function verificoClient() {
+      const storage = localStorage.getItem('user')
+      if( storage !==  null){
+          const usuario = JSON.parse(storage)
+          console.log(usuario)
+          if(usuario.mail === 'admin@borcelle.com'){
+            setClient({...usuario, admin: true })
+          } else {
+            setClient({...usuario, admin: false })
+          }
+      } 
+      
+  }
+  verificoClient()
+  }, [setClient])
 
   return (
     <div className="App">
